dca13104d8c16e49038e8c9a7ee07bc9e30490dd
ArjuMukherjee <99242994+ArjuMukherjee@users.noreply.github.com>
2026-03-11 02:39:16 +0530

    Delete sources_1/new directory

--- sources_1/new/full_adder.v
+++
@@ -1,7 +0,0 @@
-`timescale 1ns / 1ps
-module full_adder(input x, input y, input z, output s, output c);
-    wire s1, c1, c2;
-    half_adder h1(.x(x),.y(y),.s(s1),.c(c1));
-    half_adder h2(.x(z),.y(s1),.s(s),.c(c2));
-    assign c = c1 | c2;
-endmodule
--- sources_1/new/half_adder.v
+++
@@ -1,5 +0,0 @@
-`timescale 1ns / 1ps
-module half_adder(input x, input y, output s, output c);
-    assign s = x ^ y;
-    assign c = x & y;
-endmodule
